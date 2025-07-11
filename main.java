6dfd5010a51c965955fe0a230832677162110072
Mithun H S <156348229+mithunchakra@users.noreply.github.com>
2025-07-11 12:11:56 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,24 @@
+class main
+{
+
+    
+    public static void main(String[] args)
+    {
+
+       
+
+        int num1 = 20;
+        int num2 = 10;
+        
+        int temp = num1;
+        num1=num2;
+        num2 = temp;
+ 
+
+        
+        System.out.println("Swap the numbers : " + num1);
+
+        System.out.println("Swap the numbers : " + num2);
+        
+    }
+}
